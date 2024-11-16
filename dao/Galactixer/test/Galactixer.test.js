aa02202d0b14d3adf4c790215c7f2b13bf789ec9
KOSASIH <kosasihg88@gmail.com>
2024-11-16 19:12:01 +0700

    Create Galactixer.test.js

---
+++ dao/Galactixer/test/Galactixer.test.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("Galactixer Contract", function () {
+    it("Should create and vote on proposals", async function () {
+        // Test logic here
+    });
+});
