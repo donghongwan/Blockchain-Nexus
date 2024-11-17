c9fd2f2a894a1569baa64387a0ed4fb9117efa63
KOSASIH <kosasihg88@gmail.com>
2024-11-17 20:30:25 +0700

    Create Button.js

---
+++ Decentralized_Global_Bank_( DGB )/NexGen-Bank/frontend/src/components/Button.js
@@ -0,0 +1,13 @@
+// src/components/Button.js
+import React from 'react';
+import './Button.css';
+
+const Button = ({ onClick, children, disabled }) => {
+    return (
+        <button className="custom-button" onClick={onClick} disabled={disabled}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
