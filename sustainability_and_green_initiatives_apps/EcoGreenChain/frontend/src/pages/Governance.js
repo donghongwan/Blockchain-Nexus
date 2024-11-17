77a55c5a8044bd9831d3b728fdbc1020515cb686
KOSASIH <kosasihg88@gmail.com>
2024-11-17 12:02:43 +0700

    Create Governance.js

---
+++ sustainability_and_green_initiatives_apps/EcoGreenChain/frontend/src/pages/Governance.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Governance = () => {
+    return (
+        <div>
+            <h1>Governance</h1>
+            <p>Participate in governance decisions for EcoGreenChain.</p>
+            {/* Additional governance features can be added here */}
+        </div>
+    );
+};
+
+export default Governance;
