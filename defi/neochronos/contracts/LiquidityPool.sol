0948efcc006c12e26affe1adca1378e8981dedef
KOSASIH <kosasihg88@gmail.com>
2024-11-17 11:04:11 +0700

    Update LiquidityPool.sol

--- defi/neochronos/contracts/LiquidityPool.sol
+++ defi/neochronos/contracts/LiquidityPool.sol
@@ -1,6 +1,152 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract LiquidityPool {
-    // Variables and functions for liquidity management
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract LiquidityPool is Ownable, Pausable {
+    using SafeMath for uint256;
+
+    // Tokens
+    IERC20 public tokenA; // First token in the pair
+    IERC20 public tokenB; // Second token in the pair
+    IERC20 public rewardToken; // Token for rewards
+
+    // Struct to hold user liquidity information
+    struct UserLiquidity {
+        uint256 amountA; // Amount of token A
+        uint256 amountB; // Amount of token B
+        uint256 stakedAmount; // Amount of LP tokens staked
+    }
+
+    // Mapping to hold user liquidity information
+    mapping(address => UserLiquidity) public userLiquidity;
+
+    // Total liquidity in the pool
+    uint256 public totalLiquidityA;
+    uint256 public totalLiquidityB;
+
+    // Reward distribution
+    uint256 public rewardRate; // Rewards per block
+    uint256 public lastUpdateBlock; // Last block when rewards were updated
+    uint256 public totalRewards; // Total rewards distributed
+
+    event LiquidityAdded(address indexed user, uint256 amountA, uint256 amountB);
+    event LiquidityRemoved(address indexed user, uint256 amountA, uint256 amountB);
+    event RewardsClaimed(address indexed user, uint256 amount);
+    event Staked(address indexed user, uint256 amount);
+    event Unstaked(address indexed user, uint256 amount);
+
+    constructor(
+        address _tokenA,
+        address _tokenB,
+        address _rewardToken,
+        uint256 _rewardRate
+    ) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+        rewardToken = IERC20(_rewardToken);
+        rewardRate = _rewardRate;
+        lastUpdateBlock = block.number;
+    }
+
+    // Function to add liquidity
+    function addLiquidity(uint256 amountA, uint256 amountB) external whenNotPaused {
+        require(amountA > 0 && amountB > 0, "Amounts must be greater than zero");
+
+        // Transfer tokens from user to the contract
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        // Update user liquidity
+        userLiquidity[msg.sender].amountA = userLiquidity[msg.sender].amountA.add(amountA);
+        userLiquidity[msg.sender].amountB = userLiquidity[msg.sender].amountB.add(amountB);
+
+        // Update total liquidity
+        totalLiquidityA = totalLiquidityA.add(amountA);
+        totalLiquidityB = totalLiquidityB.add(amountB);
+
+        emit LiquidityAdded(msg.sender, amountA, amountB);
+    }
+
+    // Function to remove liquidity
+    function removeLiquidity(uint256 amountA, uint256 amountB) external whenNotPaused {
+        require(userLiquidity[msg.sender].amountA >= amountA, "Insufficient token A liquidity");
+        require(userLiquidity[msg.sender].amountB >= amountB, "Insufficient token B liquidity");
+
+        // Update user liquidity
+        userLiquidity[msg.sender].amountA = userLiquidity[msg.sender].amountA.sub(amountA);
+        userLiquidity[msg.sender].amountB = userLiquidity[msg.sender].amountB.sub(amountB);
+
+        // Update total liquidity
+        totalLiquidityA = totalLiquidityA.sub(amountA);
+        totalLiquidityB = totalLiquidityB.sub(amountB);
+
+        // Transfer tokens back to user
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
+
+        emit LiquidityRemoved(msg.sender, amountA, amountB);
+    }
+
+    // Function to claim rewards
+    function claimRewards() external whenNotPaused {
+        uint256 rewardAmount = calculateRewards(msg.sender);
+        require(rewardAmount > 0, "No rewards to claim");
+
+        // Update total rewards
+        totalRewards = totalRewards.add(rewardAmount);
+        lastUpdateBlock = block.number;
+
+        // Transfer rewards to user
+        rewardToken.transfer(msg.sender, rewardAmount);
+
+        emit RewardsClaimed(msg.sender, rewardAmount);
+    }
+
+    // Function to calculate rewards for a user
+    function calculateRewards(address user) internal view returns (uint256) {
+        // Implement reward calculation logic based on user liquidity and reward rate
+        // This is a placeholder for the actual calculation
+        return userLiquidity[user].amountA.add(userLiquidity[user].amountB).mul(rewardRate).div(1e18);
+    }
+
+    // Function to stake LP tokens
+    function stake(uint256 amount) external whenNotPaused {
+        require(amount > 0, "Amount must be greater than zero");
+        // Assume LP tokens are represented by a separate contract
+        // Transfer LP tokens from user to the contract
+        // lpToken.transferFrom(msg.sender, address(this), amount);
+        
+        userLiquidity[msg.sender].stakedAmount = userLiquidity[msg.sender].stakedAmount.add(amount);
+        
+        emit Staked(msg.sender, amount);
+    }
+
+    // Function to unstake LP tokens
+    function unstake(uint256 amount) external whenNotPaused {
+        require(userLiquidity[msg.sender].stakedAmount >= amount, "Insufficient staked amount");
+        
+        userLiquidity[msg.sender].stakedAmount = userLiquidity[msg.sender].stakedAmount.sub(amount);
+        // lpToken.transfer(msg.sender, amount);
+        
+        emit Unstaked(msg.sender, amount);
+    }
+
+    // Function to pause the contract
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    // Function to unpause the contract
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    // Function to set the reward rate
+    function setRewardRate(uint256 _rewardRate) external onlyOwner {
+        rewardRate = _rewardRate;
+    }
 }
