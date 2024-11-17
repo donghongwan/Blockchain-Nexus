c64cdd663a1f754f49d1d7e80701f79cd46c67ce
KOSASIH <kosasihg88@gmail.com>
2024-11-17 12:00:58 +0700

    Create Dashboard.js

---
+++ sustainability_and_green_initiatives_apps/EcoGreenChain/frontend/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Your Dashboard</h1>
+            <p>Here you can view your credits and transactions.</p>
+            {/* Additional components for displaying user data can be added here */}
+        </div>
+    );
+};
+
+export default Dashboard;
