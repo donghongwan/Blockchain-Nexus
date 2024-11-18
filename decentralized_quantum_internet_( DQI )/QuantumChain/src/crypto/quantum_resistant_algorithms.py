a8ace43c3d4608440b7291040de164dbf00b9214
KOSASIH <kosasihg88@gmail.com>
2024-11-18 10:15:25 +0700

    Create quantum_resistant_algorithms.py

---
+++ decentralized_quantum_internet_( DQI )/QuantumChain/src/crypto/quantum_resistant_algorithms.py
@@ -0,0 +1,38 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+class QuantumResistantRSA:
+    @staticmethod
+    def generate_keypair(key_size=2048):
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=key_size
+        )
+        public_key = private_key.public_key()
+        return private_key, public_key
+
+    @staticmethod
+    def encrypt(public_key, plaintext):
+        ciphertext = public_key.encrypt(
+            plaintext.encode(),
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return ciphertext
+
+    @staticmethod
+    def decrypt(private_key, ciphertext):
+        plaintext = private_key.decrypt(
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return plaintext.decode()
