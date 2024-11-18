359f4d22dde3e025da49e31cb03ac30f0c1983d6
KOSASIH <kosasihg88@gmail.com>
2024-11-18 17:53:04 +0700

    Create blockchain.py

---
+++ sustainability_and_green_initiatives_apps/ElysiumNetwork/src/core/blockchain.py
@@ -0,0 +1,36 @@
+import hashlib
+import json
+from time import time
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.create_block(previous_hash='1', proof=100)  # Genesis block
+
+    def create_block(self, proof, previous_hash):
+        block = Block(index=len(self.chain) + 1,
+                      timestamp=time(),
+                      transactions=self.current_transactions,
+                      proof=proof,
+                      previous_hash=previous_hash)
+        self.current_transactions = []  # Reset the current transactions
+        self.chain.append(block)
+        return block
+
+    def add_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def hash(self, block):
+        block_string = json.dumps(block.__dict__, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
