24e2237288fe7495517c8eeafd855b30f291d4ed
KOSASIH <kosasihg88@gmail.com>
2024-11-18 14:37:51 +0700

    Create transaction.py

---
+++ community_and_governance/MetaLedger/src/core/transaction.py
@@ -0,0 +1,14 @@
+# transaction.py
+
+class Transaction:
+    def __init__(self, sender, recipient, amount):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+
+    def to_dict(self):
+        return {
+            'sender': self.sender,
+            'recipient': self.recipient,
+            'amount': self.amount
+        }
