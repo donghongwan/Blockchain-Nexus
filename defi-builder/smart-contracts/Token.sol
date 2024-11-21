70b55dfff0504132bd118bb3157faeeadebed75c
KOSASIH <kosasihg88@gmail.com>
2024-11-21 09:12:22 +0700

    Create Token.sol

---
+++ defi-builder/smart-contracts/Token.sol
@@ -0,0 +1,18 @@
+// smart-contracts/Token.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
