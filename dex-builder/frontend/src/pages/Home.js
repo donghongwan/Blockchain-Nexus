608b7b3826215a992178a9be0dc7677f234f0bca
KOSASIH <kosasihg88@gmail.com>
2024-11-21 11:17:38 +0700

    Create Home.js

---
+++ dex-builder/frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to Advanced DEX</h2>
+            <p>Your one-stop solution for decentralized trading.</p>
+        </div>
+    );
+};
+
+export default Home;
