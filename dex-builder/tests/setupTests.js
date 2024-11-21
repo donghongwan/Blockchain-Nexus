58fdc84b2d9dd9d2af32149afd8da1d1e0b203ab
KOSASIH <kosasihg88@gmail.com>
2024-11-21 11:10:55 +0700

    Create setupTests.js

---
+++ dex-builder/tests/setupTests.js
@@ -0,0 +1,8 @@
+// tests/setupTests.js
+const Web3 = require('web3');
+const web3 = new Web3('http://localhost:8545');
+
+before(async () => {
+    // Common setup logic, e.g., deploying contracts, setting accounts
+    // ...
+});
