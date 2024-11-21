69d886ad3ec94b1b40cd9103a7dfe4e7d3f637b3
KOSASIH <kosasihg88@gmail.com>
2024-11-21 09:13:02 +0700

    Create LiquidityPool.sol

---
+++ defi-builder/smart-contracts/LiquidityPool.sol
@@ -0,0 +1,43 @@
+// smart-contracts/LiquidityPool.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LiquidityPool is Ownable {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+
+    mapping(address => uint256) public liquidity;
+
+    event LiquidityAdded(address indexed provider, uint256 amountA, uint256 amountB);
+    event LiquidityRemoved(address indexed provider, uint256 amountA, uint256 amountB);
+
+    constructor(IERC20 _tokenA, IERC20 _tokenB) {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
+
+    function addLiquidity(uint256 amountA, uint256 amountB) external {
+        require(amountA > 0 && amountB > 0, "Invalid amounts");
+
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        liquidity[msg.sender] += amountA + amountB;
+
+        emit LiquidityAdded(msg.sender, amountA, amountB);
+    }
+
+    function removeLiquidity(uint256 amountA, uint256 amountB) external {
+        require(liquidity[msg.sender] >= amountA + amountB, "Insufficient liquidity");
+
+        liquidity[msg.sender] -= amountA + amountB;
+
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
+
+        emit LiquidityRemoved(msg.sender, amountA, amountB);
+    }
+}
