8eba35df9a3d8706d51948895f6555caecd5aa29
KOSASIH <kosasihg88@gmail.com>
2024-11-21 10:55:48 +0700

    Delete dex-builder/contracts/interfaces/interfaces directory

--- dex-builder/contracts/interfaces/interfaces/IToken.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IToken {
-    function mint(address to, uint256 amount) external;
-    function burn(uint256 amount) external;
-}
