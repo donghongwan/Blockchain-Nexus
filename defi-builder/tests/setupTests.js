dffeb624037808d1ba9eefe4e288c3dd5684543c
KOSASIH <kosasihg88@gmail.com>
2024-11-21 09:31:03 +0700

    Create setupTests.js

---
+++ defi-builder/tests/setupTests.js
@@ -0,0 +1,14 @@
+// tests/setupTests.js
+const { ethers } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
