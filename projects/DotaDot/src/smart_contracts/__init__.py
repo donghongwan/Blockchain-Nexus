fce7c9bec4c88809b52924f5b454f7b3451d5bb5
KOSASIH <kosasihg88@gmail.com>
2024-11-23 10:08:33 +0700

    Create __init__.py

---
+++ projects/DotaDot/src/smart_contracts/__init__.py
@@ -0,0 +1,20 @@
+# smart_contracts/__init__.py
+
+# Import necessary contracts
+from .lending_contract import LendingContract
+from .borrowing_contract import BorrowingContract
+from .yield_farming_contract import YieldFarmingContract
+from .insurance_contract import InsuranceContract
+from .nft_marketplace_contract import NFTMarketplaceContract
+from .staking_contract import StakingContract
+from .identity_verification_contract import IdentityVerificationContract
+from .loyalty_program_contract import LoyaltyProgramContract
+from .cross_chain_bridge_contract import CrossChainBridgeContract
+from .governance_contract import GovernanceContract
+from .oracle_contract import OracleContract
+from .subscription_contract import SubscriptionContract
+from .prediction_market_contract import PredictionMarketContract
+from .quantum_contract import QuantumContract
+
+# Package version
+__version__ = "0.1.0"
