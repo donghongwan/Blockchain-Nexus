ceca9b9aa240fb19ebb288bf4ea618e02622b38a
KOSASIH <kosasihg88@gmail.com>
2024-11-23 22:57:02 +0700

    Update PriceOracle.sol

--- projects/q-mtx/contracts/PriceOracle.sol
+++ projects/q-mtx/contracts/PriceOracle.sol
@@ -2,50 +2,60 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract PriceOracle is Ownable {
-    AggregatorV3Interface[] internal priceFeeds;
-    uint256 public lastUpdateTime;
-    uint256 public updateCooldown = 1 hours; // Cooldown period for price updates
+contract PriceOracle is Ownable, Pausable {
+    using SafeMath for uint256;
 
-    // Event for price updates
-    event PriceUpdated(uint256 indexed price);
+    uint256 private currentPrice; // Price in cents
+    uint256 public lastUpdate; // Timestamp of the last price update
+    uint256 public updateDelay = 1 hours; // Delay between price updates
+    mapping(address => bool) public authorizedUpdaters; // Addresses authorized to update the price
 
-    constructor(address[] memory _priceFeeds) {
-        for (uint256 i = 0; i < _priceFeeds.length; i++) {
-            priceFeeds.push(AggregatorV3Interface(_priceFeeds[i]));
-        }
+    event PriceUpdated(uint256 newPrice, uint256 timestamp);
+    event UpdaterAuthorized(address indexed updater);
+    event UpdaterRevoked(address indexed updater);
+
+    constructor(uint256 _initialPrice) {
+        currentPrice = _initialPrice; // Set the initial price in cents
+        lastUpdate = block.timestamp; // Set the initial update time
     }
 
-    // Function to get the latest price from the first feed
-    function getLatestPrice() public view returns (uint256) {
-        (
-            , 
-            int price, 
-            , 
-            , 
-        ) = priceFeeds[0].latestRoundData();
-        require(price > 0, "Invalid price");
-        return uint256(price);
+    // Function to set the current price
+    function setCurrentPrice(uint256 _price) external whenNotPaused {
+        require(authorizedUpdaters[msg.sender] || owner() == msg.sender, "Not authorized to update price");
+        require(block.timestamp >= lastUpdate + updateDelay, "Price update too soon");
+
+        currentPrice = _price;
+        lastUpdate = block.timestamp; // Update the last update time
+        emit PriceUpdated(_price, lastUpdate);
     }
 
-    // Function to update the price feed address
-    function updatePriceFeed(address _newPriceFeed) external onlyOwner {
-        priceFeeds.push(AggregatorV3Interface(_newPriceFeed));
-        emit PriceUpdated(getLatestPrice());
+    // Function to get the current price
+    function getCurrentPrice() external view returns (uint256) {
+        return currentPrice;
     }
 
-    // Function to fetch the latest price and emit an event
-    function fetchAndEmitPrice() external {
-        require(block.timestamp >= lastUpdateTime + updateCooldown, "Cooldown period not met");
-        uint256 latestPrice = getLatestPrice();
-        lastUpdateTime = block.timestamp;
-        emit PriceUpdated(latestPrice);
+    // Function to authorize an address to update the price
+    function authorizeUpdater(address _updater) external onlyOwner {
+        authorizedUpdaters[_updater] = true;
+        emit UpdaterAuthorized(_updater);
     }
 
-    // Function to get the number of price feeds
-    function getPriceFeedCount() external view returns (uint256) {
-        return priceFeeds.length;
+    // Function to revoke an address's authorization to update the price
+    function revokeUpdater(address _updater) external onlyOwner {
+        authorizedUpdaters[_updater] = false;
+        emit UpdaterRevoked(_updater);
+    }
+
+    // Function to pause price updates
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    // Function to unpause price updates
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
