55b38521dc86bd690c9ff3f0a509ced18397c24d
KOSASIH <kosasihg88@gmail.com>
2024-11-23 15:30:06 +0700

    Create config.py

---
+++ Blockchain-Nexus-Bank-Integration/src/bank_api_clients/config.py
@@ -0,0 +1,230 @@
+import os
+
+class Config:
+    # Bank of America Configuration
+    BANK_OF_AMERICA_API_KEY = os.getenv("BANK_OF_AMERICA_API_KEY")
+    BANK_OF_AMERICA_BASE_URL = "https://api.bankofamerica.com"
+
+    # Citi Bank Configuration
+    CITI_BANK_API_KEY = os.getenv("CITI_BANK_API_KEY")
+    CITI_BANK_BASE_URL = "https://api.citibank.com"
+
+    # Wells Fargo Configuration
+    WELLS_FARGO_API_KEY = os.getenv("WELLS_FARGO_API_KEY")
+    WELLS_FARGO_BASE_URL = "https://api.wellsfargo.com"
+
+    # Chase Bank Configuration
+    CHASE_BANK_API_KEY = os.getenv("CHASE_BANK_API_KEY")
+    CHASE_BANK_BASE_URL = "https://api.chase.com"
+
+    # Capital One Configuration
+    CAPITAL_ONE_API_KEY = os.getenv("CAPITAL_ONE_API_KEY")
+    CAPITAL_ONE_BASE_URL = "https://api.capitalone.com"
+
+    # HSBC Configuration
+    HSBC_API_KEY = os.getenv("HSBC_API_KEY")
+    HSBC_BASE_URL = "https://api.hsbc.com"
+
+    # PNC Bank Configuration
+    PNC_BANK_API_KEY = os.getenv("PNC_BANK_API_KEY")
+    PNC_BANK_BASE_URL = "https://api.pnc.com"
+
+    # TD Bank Configuration
+    TD_BANK_API_KEY = os.getenv("TD_BANK_API_KEY")
+    TD_BANK_BASE_URL = "https://api.td.com"
+
+    # BBVA Configuration
+    BBVA_API_KEY = os.getenv("BBVA_API_KEY")
+    BBVA_BASE_URL = "https://api.bbva.com"
+
+    # Santander Configuration
+    SANTANDER_API_KEY = os.getenv("SANTANDER_API_KEY")
+    SANTANDER_BASE_URL = "https://api.santander.com"
+
+    # Barclays Configuration
+    BARCLAYS_API_KEY = os.getenv("BARCLAYS_API_KEY")
+    BARCLAYS_BASE_URL = "https://api.barclays.com"
+
+    # NAB Configuration
+    NAB_API_KEY = os.getenv("NAB_API_KEY")
+    NAB_BASE_URL = "https://api.nab.com.au"
+
+    # Scotiabank Configuration
+    SCOTIABANK_API_KEY = os.getenv("SCOTIABANK_API_KEY")
+    SCOTIABANK_BASE_URL = "https://api.scotiabank.com"
+
+    # Rabobank Configuration
+    RABOBANK_API_KEY = os.getenv("RABOBANK_API_KEY")
+    RABOBANK_BASE_URL = "https://api.rabobank.com"
+
+    # Standard Chartered Configuration
+    STANDARD_CHARTERED_API_KEY = os.getenv("STANDARD_CHARTERED_API_KEY")
+    STANDARD_CHARTERED_BASE_URL = "https://api.standardchartered.com"
+
+    # Credit Suisse Configuration
+    CREDIT_SUISSE_API_KEY = os.getenv("CREDIT_SUISSE_API_KEY")
+    CREDIT_SUISSE_BASE_URL = "https://api.creditsuisse.com"
+
+    # DBS Configuration
+    DBS_API_KEY = os.getenv("DBS_API_KEY")
+    DBS_BASE_URL = "https://api.dbs.com"
+
+    # UBS Configuration
+    UBS_API_KEY = os.getenv("UBS_API_KEY")
+    UBS_BASE_URL = "https://api.ubs.com"
+
+    # ING Configuration
+    ING_API_KEY = os.getenv("ING_API_KEY")
+    ING_BASE_URL = "https://api.ing.com"
+
+    # Nordea Configuration
+    NORDEA_API_KEY = os.getenv("NORDEA_API_KEY")
+    NORDEA_BASE_URL = "https://api.nordea.com"
+
+    # Bankinter Configuration
+    BANKINTER_API_KEY = os.getenv("BANKINTER_API_KEY")
+    BANKINTER_BASE_URL = "https://api.bankinter.com"
+
+    # CaixaBank Configuration
+    CAIXABANK_API_KEY = os.getenv("CAIXABANK_API_KEY")
+    CAIXABANK_BASE_URL = "https://api.caixabank.com"
+
+    # Unibanco Configuration
+    UNIBANCO_API_KEY = os.getenv("UNIBANCO_API_KEY")
+    UNIBANCO_BASE_URL = "https://api.unibanco.com"
+
+    # ABN AMRO Configuration
+    ABN_AMRO_API_KEY = os.getenv("ABN_AMRO_API_KEY")
+    ABN_AMRO_BASE_URL = "https://api.abnamro.com"
+
+    # BNP Paribas Configuration
+    BNP_PARIBAS_API_KEY = os.getenv("BNP_PARIBAS_API_KEY")
+    BNP_PARIBAS_BASE_URL = "https://api.bnpparibas.com"
+
+    # Societe Generale Configuration
+    SOCIETE_GENERALE_API _KEY = os.getenv("SOCIETE_GENERALE_API_KEY")
+    SOCIETE_GENERALE_BASE_URL = "https://api.societegenerale.com"
+
+    # Bank of New York Mellon Configuration
+    BANK_OF_NEW_YORK_MELLON_API_KEY = os.getenv("BANK_OF_NEW_YORK_MELLON_API_KEY")
+    BANK_OF_NEW_YORK_MELLON_BASE_URL = "https://api.bnymellon.com"
+
+    # Credit Agricole Configuration
+    CREDIT_AGRICOLE_API_KEY = os.getenv("CREDIT_AGRICOLE_API_KEY")
+    CREDIT_AGRICOLE_BASE_URL = "https://api.credit-agricole.com"
+
+    # Deutsche Bank Configuration
+    DEUTSCHE_BANK_API_KEY = os.getenv("DEUTSCHE_BANK_API_KEY")
+    DEUTSCHE_BANK_BASE_URL = "https://api.deutschebank.com"
+
+    # First Horizon Configuration
+    FIRST_HORIZON_API_KEY = os.getenv("FIRST_HORIZON_API_KEY")
+    FIRST_HORIZON_BASE_URL = "https://api.firsthorizon.com"
+
+    # Bank of China Configuration
+    BANK_OF_CHINA_API_KEY = os.getenv("BANK_OF_CHINA_API_KEY")
+    BANK_OF_CHINA_BASE_URL = "https://api.bankofchina.com"
+
+    # Mizuho Configuration
+    MIZUHO_API_KEY = os.getenv("MIZUHO_API_KEY")
+    MIZUHO_BASE_URL = "https://api.mizuho.com"
+
+    # Sumitomo Mitsui Configuration
+    SUMITOMO_MITSUI_API_KEY = os.getenv("SUMITOMO_MITSUI_API_KEY")
+    SUMITOMO_MITSUI_BASE_URL = "https://api.smbc.co.jp"
+
+    # KBC Bank Configuration
+    KBC_BANK_API_KEY = os.getenv("KBC_BANK_API_KEY")
+    KBC_BANK_BASE_URL = "https://api.kbc.com"
+
+    # Bank of Japan Configuration
+    BANK_OF_JAPAN_API_KEY = os.getenv("BANK_OF_JAPAN_API_KEY")
+    BANK_OF_JAPAN_BASE_URL = "https://api.boj.or.jp"
+
+    # National Australia Bank Configuration
+    NATIONAL_AUSTRALIA_BANK_API_KEY = os.getenv("NATIONAL_AUSTRALIA_BANK_API_KEY")
+    NATIONAL_AUSTRALIA_BANK_BASE_URL = "https://api.nab.com.au"
+
+    # Bank of India Configuration
+    BANK_OF_INDIA_API_KEY = os.getenv("BANK_OF_INDIA_API_KEY")
+    BANK_OF_INDIA_BASE_URL = "https://api.bankofindia.co.in"
+
+    # Standard Bank Configuration
+    STANDARD_BANK_API_KEY = os.getenv("STANDARD_BANK_API_KEY")
+    STANDARD_BANK_BASE_URL = "https://api.standardbank.com"
+
+    # Bank of Montreal Configuration
+    BANK_OF_MONTREAL_API_KEY = os.getenv("BANK_OF_MONTREAL_API_KEY")
+    BANK_OF_MONTREAL_BASE_URL = "https://api.bmo.com"
+
+    # Canadian Imperial Bank Configuration
+    CANADIAN_IMPERIAL_BANK_API_KEY = os.getenv("CANADIAN_IMPERIAL_BANK_API_KEY")
+    CANADIAN_IMPERIAL_BANK_BASE_URL = "https://api.cibc.com"
+
+    # Royal Bank of Canada Configuration
+    ROYAL_BANK_OF_CANADA_API_KEY = os.getenv("ROYAL_BANK_OF_CANADA_API_KEY")
+    ROYAL_BANK_OF_CANADA_BASE_URL = "https://api.rbc.com"
+
+    # Credit Unions Configuration
+    CREDIT_UNIONS_API_KEY = os.getenv("CREDIT_UNIONS_API_KEY")
+    CREDIT_UNIONS_BASE_URL = "https://api.creditunions.com"
+
+    # Bank of South Africa Configuration
+    BANK_OF_SOUTH_AFRICA_API_KEY = os.getenv("BANK_OF_SOUTH_AFRICA_API_KEY")
+    BANK_OF_SOUTH_AFRICA_BASE_URL = "https://api.sab.co.za"
+
+    # Bank of Nova Scotia Configuration
+    BANK_OF_NOVA_SCOTIA_API_KEY = os.getenv("BANK_OF_NOVA_SCOTIA_API_KEY")
+    BANK_OF_NOVA_SCOTIA_BASE_URL = "https://api.scotiabank.com"
+
+    # Bank of Ireland Configuration
+    BANK_OF_IRELAND_API_KEY = os.getenv("BANK_OF_IRELAND_API_KEY")
+    BANK_OF_IRELAND_BASE_URL = "https://api.bankofireland.com"
+
+    # Bank of Philippines Islands Configuration
+    BANK_OF_PHILIPPINES_ISLANDS_API_KEY = os.getenv("BANK_OF_PHILIPPINES_ISLANDS_API_KEY")
+    BANK_OF_PHILIPPINES_ISLANDS_BASE_URL = "https://api.bpi.com.ph"
+
+    # Bank of the Philippines Configuration
+    BANK_OF_THE_PHILIPPINES_API_KEY = os.getenv("BANK_OF_THE_PHILIPPINES_API_KEY")
+    BANK_OF_THE_PHILIPPINES_BASE_URL = "https://api.bpi.gov.ph"
+
+    # Bank of Thailand Configuration
+    BANK_OF_THAILAND_API_KEY = os.getenv("BANK_OF_THAILAND_API_KEY")
+    BANK_OF_THAILAND_BASE_URL = "https://api.bot.or.th"
+
+    # Bank of Singapore Configuration
+    BANK_OF_SINGAPORE_API_KEY = os.getenv("BANK_OF_SINGAPORE_API_KEY")
+    BANK_OF_SINGAPORE_BASE_URL = "https://api.bos.com.sg"
+
+    # Bank of Australia Configuration
+    BANK_OF_AUSTRALIA_API_KEY = os.getenv("BANK_OF_AUSTRALIA_API_KEY")
+    BANK_OF_AUSTRALIA_BASE_URL = "https://api.bankofaus.com.au"
+
+    # Bank of New Zealand Configuration
+    BANK_OF_NEW_ZEALAND_API_KEY = os.getenv("BANK_OF_NEW_ZEALAND_API_KEY")
+    BANK_OF_NEW_ZEALAND_BASE_URL = "https://api.bnz.co.nz"
+
+    # Bank of Norway Configuration
+    BANK_OF_NORWAY_API_KEY = os.getenv("BANK_OF_NORWAY_API_KEY")
+    BANK_OF_NORWAY_BASE_URL = "https://api.norges-bank.no"
+
+    # Bank of Denmark Configuration
+    BANK_OF_DENMARK_API_KEY = os.getenv("BANK_OF_DENMARK_API_KEY")
+    BANK_OF_DENMARK_BASE_URL = "https://api.nationalbanken.dk"
+
+    # Bank of Finland Configuration
+    BANK_OF_FINLAND_API_KEY = os.getenv("BANK_OF_FINLAND_API_KEY")
+    BANK_OF_FINLAND_BASE_URL = "https://api.suomenpankki.fi"
+
+    # Bank of Sweden Configuration
+    BANK_OF_SWEDEN_API_KEY = os.getenv("BANK_OF_SWEDEN_API_KEY")
+    BANK_OF_SWEDEN_BASE_URL = "https://api.riksbank.se"
+
+    # Bank of Iceland Configuration
+    BANK_OF_ICELAND_API_KEY = os.getenv("BANK_OF_ICELAND_API_KEY")
+    BANK_OF_ICELAND_BASE_URL = "https://api.sedlabanki.is"
+
+    # Bank of Estonia Configuration
+    BANK_OF_ESTONIA_API_KEY = os.getenv("BANK_OF_ESTONIA_API_KEY")
+    BANK_OF_ESTONIA_BASE_URL = "https://api.eestipank.ee"
