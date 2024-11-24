882f1968a33fea6235b26c1a24f74581f68f9ec0
KOSASIH <kosasihg88@gmail.com>
2024-11-24 14:48:35 +0700

    Create truffle-config.js

---
+++ projects/q-mtx/truffle-config.js
@@ -0,0 +1,44 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = process.env.MNEMONIC;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545, // Ganache GUI port
+      network_id: "*", // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${infuraKey}`),
+      network_id: 1,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0", // Specify the Solidity version
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  },
+  plugins: [
+    "truffle-plugin-verify"
+  ]
+};
