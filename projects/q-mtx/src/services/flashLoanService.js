e0c8ff09b659145d5966f2ed9c847625dca547c9
KOSASIH <kosasihg88@gmail.com>
2024-11-24 12:38:21 +0700

    Create flashLoanService.js

---
+++ projects/q-mtx/src/services/flashLoanService.js
@@ -0,0 +1,9 @@
+// services/flashLoanService.js
+class FlashLoanService {
+    requestFlashLoan(amount, purpose) {
+        // Implement flash loan logic here
+        return { message: 'Flash loan requested', amount, purpose };
+    }
+}
+
+export default new FlashLoanService();
